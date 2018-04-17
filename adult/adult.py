abc9a460a04eedd29e90abc9dd72c2616da3660f
Juan Barragan <juan@barragan.fr>
2018-04-17 23:08:59 +0200

    reorganising

--- adult/adult.py
+++ adult/adult.py
@@ -1,78 +1,69 @@
-import csv
+#!/usr/bin/python
+
 import pandas as pd
+import numpy as np
+import csv
 from sklearn.naive_bayes import GaussianNB
-#import matplotlib.pyplot as plt
-#from sklearn.metrics import roc_curve, auc
+from sklearn import tree
+import graphviz 
 
-def tokenize(file_name):
-    all_words = []
-    with open(file_name) as f:
-        tokens = dict()
-        counter = 0
-        reader = csv.reader(f)
-        for row in reader:
-            current_row_values = []
-            for entry in row:
-                if entry == ' <=50K':
-                    current_row_values.append(0)
-                    continue
-                if entry == ' >50K':
-                    current_row_values.append(1)
-                    continue
-                word = ''.join(l for l in entry if l.isalnum()).upper()
-                try:
-                    current_row_values.append(tokens[word])
-                except: 
-                    tokens[word] = counter
-                    current_row_values.append(counter)
-                    counter += 1
-            all_words.append(current_row_values)
-                
-    return tokens, pd.DataFrame(all_words)
+# Maps each category in the dataframe and it value fields to numerical values. We use bag of words method
+def get_mapper(df, categories):
+	mapper = dict()
+	for category in categories:
+		whole_set = df[category]
+		values, indices = np.unique(whole_set, return_inverse= True)
+		for i, v in enumerate(values):
+			v = v.upper().replace(' ', '')
+			mapper[(category,v)] = i
+	return mapper
 
-def tokenize_test_data(tokens, file_name):
-    all_words = []
-    with open(file_name) as f:
-        reader = csv.reader(f)
-        for row in reader:
-            current_row_values = []
-            for entry in row:
-                if entry == ' <=50K':
-                    current_row_values.append(0)
-                    continue
-                if entry == ' >50K':
-                    current_row_values.append(1)
-                    continue
-                word = ''.join(l for l in entry if l.isalnum()).upper()
-                current_row_values.append(tokens[word])
-            all_words.append(current_row_values)
-                
-    return tokens, pd.DataFrame(all_words)
+# Transform all the categorical data into numerical
+def transform_df(df, dico, categories):
+	for category in categories:
+		column = [s.upper().replace(' ','') for s in df[category]]
+		column = [s.upper().replace('.','') for s in column]
+		column = [dico[(category,v)] for v in column]
+		df[category] = column
+	return df
 
-tokens, df = tokenize('adult.data')
-y = df[df.columns[df.shape[1]-1]]
-df.drop([df.shape[1]-1], axis = 1, inplace=True)
+# Error in classification we suppose len(y) == len(y_pred)
+def error(y, y_pred):
+	s = 0
+	for i,ip in zip(y,y_pred):
+		if i!=ip: s+=1
+	return float(s)/len(y)
 
+def classifier_performance(clf, df_train, y_train, df_test, y_test):
+	clf.fit(df_train, y_train)
+	y_pred_train = clf.predict(df_train)
+	inner_error = error(y_train, y_pred_train)
+	y_pred_test = clf.predict(df_test)
+	outer_error = error(y_test, y_pred_test)
+	return inner_error, outer_error
+
+def clean_transform_ds(df, categories, dico):
+	del df['fnlwgt'] # We ignore this it has high variance, is numerical already
+	df = transform_df(df, dico, categories)
+	y = df['earnings']
+	del df['earnings']
+	return df, y
+
+# load and training and test datasets
+categories = ['workclass','education','marital','occupation','relationship','race','sex','country','earnings']
+df_train_whole = pd.read_csv('adult.data')
+dico = get_mapper(df_train_whole, categories)
+df_train, y_train = clean_transform_ds(df_train_whole, categories, dico)
+df_test_whole = pd.read_csv('adult.test')
+df_test, y_test = clean_transform_ds(df_test_whole, categories, dico)
+
+# Calculate performance for decision tree
+clf = tree.DecisionTreeClassifier(max_depth=len(categories))
+print "Deision Tree"
+print "Inner Error: %f, Outer error: %f" % classifier_performance(clf, df_train, y_train, df_test, y_test)
+
+# Calculate Performance for Naive bayes
+print "Naive Bayes"
 clf = GaussianNB()
-clf.fit(df, y)
-yp = clf.predict(df)
+print "Inner Error: %f, Outer error: %f" % classifier_performance(clf, df_train, y_train, df_test, y_test)
 
-from sklearn.metrics import precision_recall_curve
-import matplotlib.pyplot as plt
-from sklearn.metrics import average_precision_score
-
-
-y_test = y
-y_score = yp
-average_precision = average_precision_score(y_test, y_score)
-print average_precision
-precision, recall, _ = precision_recall_curve(y_test, y_score)
-plt.step(recall, precision, color='b', alpha=0.2, where='post')
-plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
-
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.ylim([0.0, 1.05])
-plt.xlim([0.0, 1.0])
-plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
-plt.show()
