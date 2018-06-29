07fa59b0522d2e5223b69c2c295f2caede1d3089
Juan Barragan <jbarragan@clearchannel.fr>
2018-06-29 10:47:41 +0200

    Documenting

--- adult/adult.py
+++ adult/adult.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# We compare Nayes Naive against tree classifier for answering the question on whether an adult
+# earns more or less than 50K Dollars a year. There are variables like
+# age,workclass,education,marital,occupation,relationship,race,sex,
+
 
 import pandas as pd
 import numpy as np
@@ -29,6 +33,7 @@ def transform_df(df, dico, categories):
 
 # Error in classification we suppose len(y) == len(y_pred)
 def error(y, y_pred):
+	assert(len(y) == len(y_pred))
 	s = 0
 	for i,ip in zip(y,y_pred):
 		if i!=ip: s+=1
@@ -43,7 +48,7 @@ def classifier_performance(clf, df_train, y_train, df_test, y_test):
 	return inner_error, outer_error
 
 def clean_transform_ds(df, categories, dico):
-	del df['fnlwgt'] # We ignore this it has high variance, is numerical already
+	del df['fnlwgt'] # We ignore this. It has high variance
 	df = transform_df(df, dico, categories)
 	y = df['earnings']
 	del df['earnings']
@@ -59,7 +64,7 @@ df_test, y_test = clean_transform_ds(df_test_whole, categories, dico)
 
 # Calculate performance for decision tree
 clf = tree.DecisionTreeClassifier(max_depth=len(categories))
-print "Deision Tree"
+print "Decision Tree"
 print "Inner Error: %f, Outer error: %f" % classifier_performance(clf, df_train, y_train, df_test, y_test)
 
 # Calculate Performance for Naive bayes
--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# We use the Titanic data set for checking up whether age and sex are good indicators for surviving. 
+# Kids and women first!?
 
 import pandas as pd
 import numpy as np
@@ -9,6 +11,9 @@ from sklearn.model_selection import train_test_split
 import scipy.optimize
 import operator
 from sklearn import tree
+
+# Given a data frame, a covariate represented by category_name and its classification represented by label_name,
+# we calculate the gini coefficient or the total impurity of the classification
 
 def gini(df, category_name, category_values, label_name, labels_values):
     category_cardinalities = { 
@@ -26,11 +31,14 @@ def gini(df, category_name, category_values, label_name, labels_values):
         gini += category_cardinalities[category]/float(len(df))*(1 - s/float(category_cardinalities[category])**2)
     return gini    
 
+# Sets out the age covariate
 def gini_by_age(df, t):
     df['age_group'] = df['age'].apply(lambda row : 'child' if row <= t else 'adult')
     g = gini(df, 'age_group', ['child', 'adult'], 'survived', [0,1])
     return g
 
+# Plots the gini coefficient for the age when the age varies in order to find the best age cut.
+# It is a discontinous function but we get the point nonetheless
 def plot_gini_age(df, t0, t1, step, fname):
     ages = np.arange(t0, t1, step)
     y = [gini_by_age(titanic_df, a) for a in ages]
@@ -41,6 +49,7 @@ def plot_gini_age(df, t0, t1, step, fname):
     min_index, min_value = min(enumerate(y), key=operator.itemgetter(1))
     return ages[min_index], min_value
 
+# We verify using the scikit framework
 def use_tree(df, fname):
     X = np.array(df['age']).reshape((len(df['age']),1))
     y = df['survived']
@@ -56,9 +65,9 @@ sex_cov = titanic_df[['sex', 'survived']]
 print gini(sex_cov, 'sex', ['female', 'male'], 'survived',[0,1])
 # Now check the age
 age_cov = titanic_df[['age', 'survived']].dropna()
-age, min_index = plot_gini_age(age_cov, 4, 20, 0.2, '../../book/gini_age.eps')
+age, min_index = plot_gini_age(age_cov, 4, 20, 0.2, './gini_age.eps')
 print "At age %f, the Gini Index is %f" %(age, min_index)
-use_tree(age_cov, '../../book/graph.pdf')
+use_tree(age_cov, './graph')
 
 
 
